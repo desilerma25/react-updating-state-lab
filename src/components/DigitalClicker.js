e2ac5f169837c5093eb0accbfb94618de25702c0
Desiree Lerma <desi.lerma96@yahoo.com>
2021-03-01 19:47:42 -0600

    commit tree c5c77295223f89596d4db48068b86f52d83d65d7

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+
+    state = {
+        timesClicked: 0
+    }
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            timesClicked: prevState.timesClicked + 1
+        }))
+    }
+  render() {
+      return(
+          <div>
+              <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+          </div>
+      )
+  }  
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,50 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+            }
+        }
+    }
+
+    changeBitrate = () => {
+        this.setState({
+            settings: {
+                // use spread to leave other settings intact
+                // prevents overwriting and eliminating other k/v pairs   
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    changeResolution = () => {
+        this.setState({
+            // use spread, leave unchanged key/values in tact
+            settings: {
+                ...this.state.settings,
+                video: {
+                    // go into video, use spread once again to get into settings
+                    ...this.state.settings.video,
+                    resolution: '720p'
+                }
+            }
+
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <button className='bitrate' onClick={this.changeBitrate}>Update Bitrate</button>
+                <button className='resolution' onClick={this.changeResolution}>Update Resolution</button>
+            </div>
+        )
+    }
+}
